5577c664941a118e6dac912e9dec63b4445da959
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-06-20 22:47:55 -0300

    Threads

---
+++ Threads/src/aula67/Main.java
@@ -0,0 +1,24 @@
+package aula67;
+
+public class Main {
+
+	/*Multitarefas: processos e threads
+	 * Processo é um programa que está sendo executado
+	 * Thread é a menor unidade de código que pode ser executado
+	 *   ou seja, permite ao programa executar 2 ou mais tarefas ao mesmo tempo
+	 * Exemplo: editor de texto: formatar e imprimir ao mesmo tempo
+	 */
+	/*Ciclo de vida
+	 * New: new Thread()
+	 * Runnable: start() ----notify()/notifyAll()/resume()
+	 * Waiting: sleep()/wait()/suspend()
+	 * Running run()/yield()
+	 * Dead
+	 * 
+	 */
+	/*start: inicia
+	 * run: executa a tarefa da thread
+	 * sleep: coloca a thread para dormir por x millisegundos
+	 * 
+	 */
+}
---
+++ Threads/src/aula67/Teste.java
@@ -0,0 +1,12 @@
+package aula67;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		MinhaThread thread = new  MinhaThread("Thread #1",900);
+		MinhaThread thread2 = new  MinhaThread("Thread #2",1500);
+		//thread.start();
+	}
+
+}
