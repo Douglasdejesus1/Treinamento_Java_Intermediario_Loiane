e97b12fdbff95a587c4df74c91e1fbec457fc4b6
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-06-24 07:54:11 -0300

    Math

---
+++ Math/src/aula85_100/Main.java
@@ -0,0 +1,26 @@
+package aula85_100;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println(Math.pow(2,3));
+		
+		System.out.println(Math.round(4.46));
+		System.out.println(Math.round(4.76));
+		
+		System.out.println(Math.ceil(4.76));
+		System.out.println(Math.ceil(4.46));
+		
+		System.out.println(Math.floor(4.46));
+		System.out.println(Math.floor(4.76));
+		
+		System.out.println(Math.round(Math.random()*100));
+		
+		System.out.println(Math.sin(Math.toRadians(30)));
+		System.out.println(Math.cos(Math.toRadians(1)));
+		System.out.println(Math.tan(Math.toRadians(45)));
+	}
+	
+
+}
